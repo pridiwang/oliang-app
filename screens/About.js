1dc18977268d3a3ca9b03a56f03a7fe9d4f980f3
pridiwang <preedeew@gmail.com>
2018-06-05 16:13:52 +0700

    build31

--- screens/About.js
+++ screens/About.js
@@ -1,10 +1,12 @@
 import React  from 'react';
 import PropTypes from 'prop-types';
-import {View,Text,Image,StyleSheet,TouchableHighlight,ScrollView,Button}  from 'react-native';
+import {View,Text,Image,StyleSheet,TouchableHighlight,ScrollView,Button,Platform}  from 'react-native';
 import Loginscreen from './Login';
 import {themeLight,themeDark} from './Styles';
 import HTMLView from 'react-native-htmlview';
 import VersionNumber from 'react-native-version-number';
+import MyWebView from 'react-native-webview-autoheight';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 function renderNode(node, index, siblings, parent, defaultRenderer) {
     if (node.name == 'iframe') {
       return null;
@@ -12,7 +14,7 @@ function renderNode(node, index, siblings, parent, defaultRenderer) {
   }
 const HTMLcontent='<h3>เกี่ยวกับ(About)แอปพลิเคชัน“โอเลี้ยง (Oliang)”</h3><p>แอปพลิเคชัน“โอเลี้ยง (Oliang)”นี้ จัดทำขึ้นเพื่อประโยชน์ในการสื่อสารภายในสำนักงาน กสทช. เป็นหลัก แต่สำหรับเนื้อหาบางส่วนที่สามารถเผยแพร่ต่อสาธารณชนในวงกว้างได้ก็จะจัดให้มีการเผยแพร่ต่อสาธารณชนได้ด้วย โดยแอปพลิเคชันนี้อยู่บนพื้นฐานแนวคิดที่ว่า “ข้อมูลภายใน คนในต้องรู้ก่อน” ทั้งนี้ โดยสาระสำคัญหลักของแอปพลิเคชัน “โอเลี้ยง” นี้ จะมุ่งเนนให้เป็นสื่อ (Media) ที่ส่งผ่านเนื้อหาที่เป็นบทความ ข่าวสาร ข้อมูลสถิติ ตลอดจนข้อสั่งการของผู้บริหารระดับสูงที่ต้องการส่งถึงบุคลากรภายในสำนักงาน กสทช. ได้รับทราบโดยตรง และมีลักษณะ  “เฉพาะคนใน” เท่านั้นที่จะสามารถรับรู้ รับทราบ เพื่อให้เกิดการแยกแยะช่องทางสื่อสารข้อมูลสำคัญซึ่งจะต้องส่งต่อออกไปยังบุคคลภายนอกสำนักงาน กสทช. ได้ยากกว่าการใช้การสื่อสารผ่านสื่อสังคมออนไลน์อื่นๆ ที่อาจส่งต่อไปยังบุคคลภายนอกสำนักงาน กสทช. ได้โดยสะดวกมากกว่า ซึ่งเป็นเงื่อนไขสำคัญที่ระบบแอปพลิเคชัน “โอเลี้ยง” จึงต้องกำหนดให้บุคลากรของสำนักงาน กสทช. ต้องลงทะเบียนเข้าใช้งานด้วย ข้อมูลลงทะเบียนเพื่อการเข้าถึงและใช้งานระบบภายในสำนักงาน กสทช. (Access Directory:AD) ซึ่งจะทำให้เข้าถึงหัวข้อต่างๆ ได้มากกว่าผู้ใช้งานทั่วไปที่ต้องลงทะเบียนด้วยบัญชี Facebook โดยระบบแอปพลิเคชัน “โอเลี้ยง” จะมีการจัดเก็บข้อมูลการ Log In และการเปิดอ่านข้อมูลแต่ละเรื่อง (Subject) เพื่อจัดเก็บเป็นสถิติสำหรับใช้ในการพัฒนา ปรับปรุง และดำเนินการตามที่สำนักงาน กสทช. เห็นสมควรต่อไป </p><h3>วัตถุประสงค์ของแอปพลิเคชั่น (Objective)</h3><ul><li>เพื่อพัฒนาแอปพลิเคชันสำหรับใช้สื่อสารข้อมูลสำคัญของสำนักงาน กสทช. โดยส่งต่อถึงบุคลากรภายในสำนักงาน กสทช.ได้โดยสะดวกรวดเร็ว และเป็นการลดขั้นตอนการสั่งงานตามสายบังคับบัญชาให้มีความกระชับมากยิ่งขึ้นด้วย<br><li>เพื่อเป็นช่องทางในการสื่อสาร และประชาสัมพันธ์ผลงานของสำนักงาน กสทช.ที่มีลักษณะเป็นข้อมูลทางการ ให้ประชาชนทั่วไปสามารถเข้าถึงได้อย่างสะดวกและรวดเร็วมากยิ่งขึ้น </ul><h3>ที่ปรึกษาโครงการ</h3><p>นายฐากร	ตัณฑสิทธิ์<br> -เลขาธิการ กสทช. <h3>ผู้อำนวยการโครงการ</h3><p>นายก่อกิจ ด่านชัยวิจิตร <br> - รองเลขาธิการ กสทช.<br>นางสาวอรวรี เจริญพร <br> - รักษาการ ผู้อำนวยการสำนัก <h3>ผู้ดำเนินการโครงการและผู้ประสานงาน</h3> <p>นางสาวอรวรี เจริญพร <br>นายพิศล สุ่นกุล <br>นายณัฐกิตติ์ สุริยะไกร <h3>ผู้พัฒนา</h3> <p>ห้างหุ้นส่วนจำกัด แฟเวอร์ริทดีไซน์ ';
 export default class AboutScreen extends React.Component{
-    static navigationOptions = {
+      static navigationOptions=({navigation})=>({
         drawerLabel: 'เกี่ยวกับโอเลี้ยง',
         drawerIcon: ({ tintColor }) => (
           <Image
@@ -20,26 +22,23 @@ export default class AboutScreen extends React.Component{
             style={[styles.icon, {tintColor: tintColor}]}
           />
         ),
-        header:null,
-      };
-    
+        title:'About',
+        headerStyle:{marginTop: Platform.OS ==='ios' ? 0 : -30 },
+        headerLeft: <TouchableHighlight onPress={()=>navigation.navigate('Category',)}><Ionicons name="md-arrow-back" style={styles.topbtn} size={32} color="green" /></TouchableHighlight>,
+        headerRight:<TouchableHighlight onPress={()=>navigation.navigate('Category',)}><Ionicons name="md-home" style={styles.topbtn} size={32} color="green" /></TouchableHighlight>,
+      });
+
+
     render(){
-    
+    //<ScrollView style={{flex:1}}></ScrollView>
         return(
-            <ScrollView style={styles.container}>
-            <View style={{margin:10,padding:10,flex:1,flexDirection:'column',alignContent:'center',alignItems:'center'}} >
-            <View style={{flex:1,flexDirection:'row',justifyContent:'flex-start'}}><Button title="<Back" onPress={()=>this.props.navigation.navigate('Category')} style={{}}/>
-              </View>
-            <Image source={require ('../img/oliang-text.png')} style={{height:80,width:200}} />
             
-         <HTMLView styles={{width:320,flex:5,padding:10,margin:0,fontSize:16,backgroundColor:'#dddddd'}} stylesheet={htmlStyles} value={HTMLcontent} ></HTMLView>
-            </View>
-            <Text style={{textAlign:"center",margin:5,color:'#666666'}}>หมายเลขรุ่น 1.04 (26) </Text>
-            <Text style={{textAlign:"center",margin:5,color:'#666666'}}>วันที่อัพเดทล่าสุด 22 พฤษภาคม 2561 </Text>
-            <View style={{padding:10,margin:10,alignItems:'center'}} >
-            <Image source={require ('../img/appicon4_512.png')} style={{margin:5,height:100,width:100}} />
-            </View>
-            </ScrollView>
+              <MyWebView hasZoom='true' source={{uri:'http://oliang.itban.com/about.html',method:'GET'}} 
+                        style={{marginTop:10,marginRight:10,padding:20,flex:2,backgroundColor:'rgba(0,0,0,0)' }} 
+                        scrollEnabled={true}
+                        />
+            
+            
         )
     }
 }
